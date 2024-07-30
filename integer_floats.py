930ef2058892d11ad1e564036e389dd0e03ba693
Sundar Khanal <sundarkhanal62@gmail.com>
2024-07-30 22:25:51 +0545

    Second day

---
+++ integer_floats.py
@@ -0,0 +1,36 @@
+num = 3
+print(type(num))
+
+
+print(3//2) #removes float value
+print(3**2)  #gives power value
+print(3/2)  #gives remainder
+
+print(3*3+4)
+print(3+(3*4)) #order of operation is followed here
+
+num  = 1
+num *= 10
+num /= 10
+print(num) #prints 1.0
+print(num) #num is now 10
+
+
+print(round(3.76))
+print(round(3.76,1))  # 1represents- the first digit we want to round after decimal
+
+
+#comparasion operators
+num_1 = 3
+num_2 = 5
+print(num_1 == num_2) #checks if both are equal
+print(num_1 != num_2) #checks if both are not equal
+print(num_1>num_2)
+print(num_1<num_2)
+
+
+num_1 = '3'
+num_2 = '5'
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1+num_2)
